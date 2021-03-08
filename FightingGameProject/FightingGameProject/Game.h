d31edc9ab4fef5e290f34a9e50ef5c8d0724ebc8
Choi <roundbeargames@gmail.com>
2021-03-08 15:00:43 +0900

    fixedtimestep

--- FightingGameProject/FightingGameProject/Game.h
+++ FightingGameProject/FightingGameProject/Game.h
@@ -7,22 +7,31 @@ namespace RB
 	class Game : public olc::PixelGameEngine
 	{
 	private:
+		float fTargetFrameTime = 1.0f / 120.0f; // target fixed timestep
+		float fAccumulatedTime = 0.0f;
 
 	public:
 		bool OnUserCreate() override
 		{
-			sAppName = "ShitFall";
+			sAppName = "C++FightingGame";
 			return true;
 		}
 
 		bool OnUserUpdate(float fElapsedTime) override
 		{
+			//fixed timestep
+			if (fAccumulatedTime >= fTargetFrameTime)
+			{
+				fAccumulatedTime -= fTargetFrameTime;
+				fElapsedTime = fTargetFrameTime;
+			}
+
 			return true;
 		}
 
 		void Run()
 		{
-			if (Construct(1920, 1080, 1, 1))
+			if (Construct(854, 480, 1, 1))
 			{
 				Start();
 			}
