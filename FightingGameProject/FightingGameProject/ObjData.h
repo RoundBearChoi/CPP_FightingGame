c1c35c8e90b3969e2af5dc8e91d541aff4a8e02d
Choi <roundbeargames@gmail.com>
2021-03-08 22:33:32 +0900

    lines

--- FightingGameProject/FightingGameProject/ObjData.h
+++ FightingGameProject/FightingGameProject/ObjData.h
@@ -7,30 +7,17 @@ namespace RB
 	{
 	private:
 		olc::vf2d position = { 0.0f, 0.0f };
+		olc::vf2d spriteSize = { 0.0f, 0.0f };
 		size_t creationID = 0;
 
 	public:
-		olc::vf2d spriteSize = { 0.0f, 0.0f };
+		olc::vf2d GetPosition() { return position; }
+		void SetPosition(float _x, float _y) { position.x = _x; position.y = _y; }
 
-		olc::vf2d GetPosition()
-		{
-			return position;
-		}
+		olc::vf2d GetSpriteSize() { return spriteSize; }
+		void SetSpriteSize(float _x, float _y) { spriteSize.x = _x; spriteSize.y = _y; };
 
-		void SetPosition(float _x, float _y)
-		{
-			position.x = _x;
-			position.y = _y;
-		}
-
-		size_t GetCreationID()
-		{
-			return creationID;
-		}
-
-		void SetCreationID(size_t t)
-		{
-			creationID = t;
-		}
+		size_t GetCreationID() { return creationID;	}
+		void SetCreationID(size_t t) { creationID = t; }
 	};
 }
