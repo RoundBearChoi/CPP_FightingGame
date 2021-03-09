dec209a6e6b528046e6c75e12bc3ca6f45f3ae70
Choi <roundbeargames@gmail.com>
2021-03-09 13:44:53 +0900

    0, 0 position

--- FightingGameProject/FightingGameProject/FightScene.h
+++ FightingGameProject/FightingGameProject/FightScene.h
@@ -45,15 +45,15 @@ namespace RB
 			float zoomScale = cam.GetZoom();
 
 			//centermark
-			olc::vi2d bottom{ 0, 0 };
-			olc::vi2d top{ 0, -10 };
-			olc::vi2d left{ -10, 0 };
-			olc::vi2d right{ 10, 0 };
+			olc::vi2d bottom{ 0, 7 };
+			olc::vi2d top{ 0, -7 };
+			olc::vi2d left{ -7, 0 };
+			olc::vi2d right{ 7, 0 };
 
 			olc::vi2d relativeTop = RelativeVector::Get(top, camPos, zoomScale);
 			olc::vi2d relativeBottom = RelativeVector::Get(bottom, camPos, zoomScale);
-			olc::vi2d relativeLeft = RelativeVector::Get(bottom + left, camPos, zoomScale);
-			olc::vi2d relativeRight = RelativeVector::Get(bottom + right, camPos, zoomScale);
+			olc::vi2d relativeLeft = RelativeVector::Get(left, camPos, zoomScale);
+			olc::vi2d relativeRight = RelativeVector::Get(right, camPos, zoomScale);
 
 			ptrEngine->DrawLine(relativeTop, relativeBottom, olc::GREEN);
 			ptrEngine->DrawLine(relativeLeft, relativeRight, olc::GREEN);
