5d0d9e4b96de96a1af67d3fcc844bd8c66714ebf
Choi <roundbeargames@gmail.com>
2021-03-21 00:13:27 +0900

    ExperimentalLoader.cpp

---
+++ FightingGameProject/ExperimentalLoader.cpp
@@ -0,0 +1,67 @@
+#include "ExperimentalLoader.h"
+
+namespace RB
+{
+	ExperimentalLoader* ExperimentalLoader::ptr;
+
+	ExperimentalLoader::ExperimentalLoader()
+	{
+
+	}
+
+	ExperimentalLoader::~ExperimentalLoader()
+	{
+		DeleteDecals(backgroundSprites, backgroundDecals);
+	}
+
+	void ExperimentalLoader::LoadSprites(std::string path, std::vector<olc::Sprite*>& vecSprites, std::vector<olc::Decal*>& vecDecals, std::vector<size_t>& vecHash)
+	{
+		//load sprites
+		for (const auto& i : FILESYSTEM::directory_iterator(path))
+		{
+			FILESYSTEM::path path = i.path();
+
+			std::cout << "Loading sprite: " << path << std::endl;
+
+			std::string fileName = path.u8string();
+			vecSprites.push_back(new olc::Sprite(fileName));
+
+			size_t hash = std::hash<std::string>{}(fileName);
+			//size_t testhash = std::hash<std::string>{}("PNG files\\Background\\background_mountains.png");
+
+			std::cout << hash << std::endl;
+			//std::cout << testhash << std::endl;
+
+			vecHash.push_back(hash);
+		}
+
+		//load decals
+		for (size_t i = 0; i < vecSprites.size(); i++)
+		{
+			IF_COUT{ std::cout << "Loading decal: " << i << std::endl; }
+
+			vecDecals.push_back(new olc::Decal(vecSprites[i]));
+		}
+	}
+
+	void ExperimentalLoader::LoadBackgroundSprites()
+	{
+		std::string path = "PNG files/Background";
+		LoadSprites(path, backgroundSprites, backgroundDecals, backgroundHash);
+	}
+
+	void ExperimentalLoader::DeleteDecals(std::vector<olc::Sprite*>& spriteVec, std::vector<olc::Decal*>& decalVec)
+	{
+		for (size_t i = 0; i < spriteVec.size(); i++)
+		{
+			std::cout << "deleting sprite: " << backgroundHash[i] << std::endl;
+			delete spriteVec[i];
+		}
+
+		for (size_t i = 0; i < decalVec.size(); i++)
+		{
+			std::cout << "deleting decal: " << backgroundHash[i] << std::endl;
+			delete decalVec[i];
+		}
+	}
+}
--- FightingGameProject/ExperimentalLoader.h
+++ FightingGameProject/ExperimentalLoader.h
@@ -2,6 +2,7 @@
 #include <vector>
 #include <iostream>
 #include "olcPixelGameEngine.h"
+#include "DevSettings.h"
 
 #ifdef _WIN32
 //filesystem not included in VS (strange..)
@@ -21,57 +22,15 @@ namespace RB
 		std::vector<olc::Decal*> backgroundDecals;
 		std::vector<size_t> backgroundHash;
 
+		void LoadSprites(std::string path, std::vector<olc::Sprite*>& vecSprites, std::vector<olc::Decal*>& vecDecals, std::vector<size_t>& vecHash);
+
 	public:
-		~ExperimentalLoader()
-		{
-			DeleteDecals(backgroundSprites, backgroundDecals);
-		}
+		static ExperimentalLoader* ptr;
 
-		void LoadBackgroundSprites()
-		{
-			std::string path = "PNG files/Background";
+		ExperimentalLoader();
+		~ExperimentalLoader();
 
-			//load sprites
-			for (const auto& i : FILESYSTEM::directory_iterator(path))
-			{
-				FILESYSTEM::path path = i.path();
-				
-				std::cout << "Loading sprite: " << path << std::endl;
-
-				std::string fileName = path.u8string();
-				backgroundSprites.push_back(new olc::Sprite(fileName));
-
-				size_t hash = std::hash<std::string>{}(fileName);
-				//size_t testhash = std::hash<std::string>{}("PNG files\\Background\\background_mountains.png");
-
-				std::cout << hash << std::endl;
-				//std::cout << testhash << std::endl;
-
-				backgroundHash.push_back(hash);
-			}
-
-			//load decals
-			for (size_t i = 0; i < backgroundSprites.size(); i++)
-			{
-				IF_COUT{ std::cout << "Loading decal: " << i << std::endl; }
-
-				backgroundDecals.push_back(new olc::Decal(backgroundSprites[i]));
-			}
-		}
-
-		void DeleteDecals(std::vector<olc::Sprite*>& spriteVec, std::vector<olc::Decal*>& decalVec)
-		{
-			for (size_t i = 0; i < spriteVec.size(); i++)
-			{
-				std::cout << "deleting sprite: " << backgroundHash[i] << std::endl;
-				delete spriteVec[i];
-			}
-
-			for (size_t i = 0; i < decalVec.size(); i++)
-			{
-				std::cout << "deleting decal: " << backgroundHash[i] << std::endl;
-				delete decalVec[i];
-			}
-		}
+		void LoadBackgroundSprites();
+		void DeleteDecals(std::vector<olc::Sprite*>& spriteVec, std::vector<olc::Decal*>& decalVec);
 	};
 }
