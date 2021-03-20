2c81ef46ec5336231a627e3a2013e545871989ee
Choi <roundbeargames@gmail.com>
2021-03-21 01:36:46 +0900

    cout

--- FightingGameProject/Background_Idle_Sun.h
+++ FightingGameProject/Background_Idle_Sun.h
@@ -14,8 +14,11 @@ namespace RB
 		{
 			static size_t hash = 0;
 
+			IF_COUT{ std::cout << "sun initial hash: " << hash << std::endl; }
+
 			if (hash == 0)
 			{
+				IF_COUT{ std::cout << "hashing: " << path << std::endl; }
 				hash = std::hash<std::string>{}(path);
 			}
 
