f930acf8bbc7f0c2686d65ed59fa112256b7273b
Choi <roundbeargames@gmail.com>
2021-03-22 21:28:25 +0900

    test

--- FightingGameProject/ColliderLoader.h
+++ FightingGameProject/ColliderLoader.h
@@ -60,10 +60,15 @@ namespace RB
 							float rotation = 0.0f;
 
 							std::fread(&x, sizeof(int32_t), 1, pFile);
+							std::fflush(pFile);
 							std::fread(&y, sizeof(int32_t), 1, pFile);
+							std::fflush(pFile);
 							std::fread(&width, sizeof(int32_t), 1, pFile);
+							std::fflush(pFile);
 							std::fread(&height, sizeof(int32_t), 1, pFile);
+							std::fflush(pFile);
 							std::fread(&rotation, sizeof(float), 1, pFile);
+							std::fflush(pFile);
 
 							vecColliders[i].SetPosition(x, y);
 							vecColliders[i].SetWidth(width);
@@ -116,13 +121,13 @@ namespace RB
 						float rotation = vecColliders[i].Rotation();
 
 						fwrite(&x, sizeof(int32_t), 1, pFile);
-						fflush(pFile);
+						std::fflush(pFile);
 						fwrite(&y, sizeof(int32_t), 1, pFile);
-						fflush(pFile);
+						std::fflush(pFile);
 						fwrite(&width, sizeof(int32_t), 1, pFile);
-						fflush(pFile);
+						std::fflush(pFile);
 						fwrite(&height, sizeof(int32_t), 1, pFile);
-						fflush(pFile);
+						std::fflush(pFile);
 						fwrite(&rotation, sizeof(float), 1, pFile);
 						fflush(pFile);
 
