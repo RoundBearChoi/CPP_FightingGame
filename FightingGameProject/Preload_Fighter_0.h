040b2907055db52869b206409307b8a8ed5c53a2
Choi <roundbeargames@gmail.com>
2021-03-26 23:39:42 +0900

    preloading collider data

--- FightingGameProject/Preload_Fighter_0.h
+++ FightingGameProject/Preload_Fighter_0.h
@@ -9,10 +9,22 @@ namespace RB
 {
 	class Preload_Fighter_0
 	{
+	private:
+		template<class T>
+		void Load()
+		{
+			T state;
+			state.UpdateColliders();
+		}
+
 	public:
 		Preload_Fighter_0()
 		{
-
+			Load<Fighter_0_Idle>();
+			Load<Fighter_0_WalkForward>();
+			Load<Fighter_0_WalkBack>();
+			Load<Fighter_0_Jab>();
+			Load<Fighter_0_HitReaction_0>();
 		}
 	};
 }
