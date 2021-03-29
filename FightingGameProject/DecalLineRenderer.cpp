89881d7bd3d0625adeb020ad27ddf34c8ce728e9
Choi <roundbeargames@gmail.com>
2021-03-29 17:47:42 +0900

    Revert "line renderer test"
    This reverts commit ad7275d4704b12dbc671a2651cc227e02d70aac8.

--- FightingGameProject/DecalLineRenderer.cpp
+++
@@ -1,8 +0,0 @@
-#include "DecalLineRenderer.h"
-
-namespace RB
-{
-	std::string DecalLineRenderer::path = "PNG files/DebugElements/245whitesq.png";
-	olc::vi2d DecalLineRenderer::sourcePos = { 0, 0 };
-	olc::vi2d DecalLineRenderer::sourceSize = { 245, 245 };
-}
