1b45b24728554f5bb606d534329bd384570c626e
Choi <roundbeargames@gmail.com>
2021-04-01 12:18:26 +0900

    rename

--- FightingGameProject/DiagonalOverlap.h
+++ FightingGameProject/DiagonalOverlap.h
@@ -59,13 +59,13 @@ namespace RB
 	public:
 		static bool IsColliding(GameObj& me, BodyType& myBody, GameObj& enemy, BodyType& enemyBody)
 		{
-			olc::vi2d p1col = me.stateController.currentState->GetColliderWorldPos(myBody, me.objData);
+			olc::vi2d p1Center = me.stateController.currentState->GetColliderWorldPos(myBody, me.objData);
 			std::array<olc::vi2d, 4> p1Quads = me.stateController.currentState->GetColliderQuadsWorldPos(myBody, me.objData);
 
-			olc::vi2d p2col = enemy.stateController.currentState->GetColliderWorldPos(enemyBody, enemy.objData);
+			olc::vi2d p2Center = enemy.stateController.currentState->GetColliderWorldPos(enemyBody, enemy.objData);
 			std::array<olc::vi2d, 4> p2Quads = enemy.stateController.currentState->GetColliderQuadsWorldPos(enemyBody, enemy.objData);
 
-			if (DiagonalOverlap::yes(p1col, p1Quads, p2col, p2Quads))
+			if (DiagonalOverlap::yes(p1Center, p1Quads, p2Center, p2Quads))
 			{
 				return true;
 			}
