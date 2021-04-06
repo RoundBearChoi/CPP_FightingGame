a984fb90e6fcdaab2308e2ef3f608fdffdae32b3
Choi <roundbeargames@gmail.com>
2021-04-06 20:04:10 +0900

    projectilegroup.cpp, impacteffectsgroup.h

---
+++ FightingGameProject/ProjectileGroup.cpp
@@ -0,0 +1,202 @@
+#include "ProjectileGroup.h"
+
+namespace RB
+{
+	ProjectileGroup::~ProjectileGroup()
+	{
+		if (vecObjs.size() != 0)
+		{
+			IF_COUT{ std::cout << std::endl; };
+
+			for (size_t i = 0; i < vecObjs.size(); i++)
+			{
+				IF_COUT{ std::cout << "destructing projectile: " << vecObjs[i]->objData.GetCreationID() << std::endl; };
+				delete vecObjs[i];
+			}
+
+			IF_COUT{ std::cout << std::endl; };
+		}
+	}
+
+	void ProjectileGroup::UpdateStates(GameData& gameData)
+	{
+		for (size_t i = 0; i < vecObjs.size(); i++)
+		{
+			if (vecObjs[i] != nullptr)
+			{
+				GameObj& obj = *vecObjs[i];
+				State* state = vecObjs[i]->stateController.currentState;
+
+				if (state != nullptr)
+				{
+					state->RunUpdateProcess(vecObjs[i]->objData, gameData);
+				}
+			}
+		}
+	}
+
+	void ProjectileGroup::RenderObjPosition(Camera& cam)
+	{
+		for (size_t i = 0; i < vecObjs.size(); i++)
+		{
+			if (vecObjs[i] != nullptr)
+			{
+				vecObjs[i]->objData.objBoxCollider.Render(cam, vecObjs[i]->objData.GetPosition(), olc::CYAN);
+			}
+		}
+	}
+
+	void ProjectileGroup::RenderStates(Camera& cam, bool update)
+	{
+		for (size_t i = 0; i < vecObjs.size(); i++)
+		{
+			if (vecObjs[i] != nullptr)
+			{
+				SheetRenderer::Render(vecObjs[i], cam);
+
+				if (update)
+				{
+					vecObjs[i]->stateController.currentState->animationController.NextTileIndex();
+				}
+			}
+		}
+	}
+
+	void ProjectileGroup::RenderBoxColliders(Camera& cam)
+	{
+
+	}
+
+	olc::vi2d ProjectileGroup::GetObjWorldPos(size_t index)
+	{
+		if (index < vecObjs.size())
+		{
+			if (vecObjs[index] != nullptr)
+			{
+				return vecObjs[index]->objData.GetPosition();
+			}
+		}
+
+		return { 0, 0 };
+	}
+
+	olc::vi2d ProjectileGroup::GetObjBoxColliderWorldPos(size_t index)
+	{
+		if (index < vecObjs.size())
+		{
+			if (vecObjs[index] != nullptr)
+			{
+				olc::vi2d relativePos = vecObjs[index]->objData.objBoxCollider.RelativePosition();
+				olc::vi2d worldPos = relativePos + vecObjs[index]->objData.GetPosition();
+				return worldPos;
+			}
+		}
+
+		return { 0, 0 };
+	}
+
+	std::array<olc::vi2d, 4> ProjectileGroup::GetObjBoxColliderWorldQuad(size_t index)
+	{
+		std::array<olc::vi2d, 4> arr;
+
+		if (index < vecObjs.size())
+		{
+			if (vecObjs[index] != nullptr)
+			{
+				arr[0] = vecObjs[index]->objData.objBoxCollider.RelativePoint0();
+				arr[1] = vecObjs[index]->objData.objBoxCollider.RelativePoint1();
+				arr[2] = vecObjs[index]->objData.objBoxCollider.RelativePoint2();
+				arr[3] = vecObjs[index]->objData.objBoxCollider.RelativePoint3();
+
+				olc::vi2d relativePos = vecObjs[index]->objData.objBoxCollider.RelativePosition();
+
+				arr[0] += relativePos;
+				arr[1] += relativePos;
+				arr[2] += relativePos;
+				arr[3] += relativePos;
+
+				olc::vi2d playerPos = vecObjs[index]->objData.GetPosition();
+
+				arr[0] += playerPos;
+				arr[1] += playerPos;
+				arr[2] += playerPos;
+				arr[3] += playerPos;
+			}
+		}
+
+		return arr;
+	}
+
+	size_t ProjectileGroup::GetObjCount()
+	{
+		return vecObjs.size();
+	}
+
+	size_t ProjectileGroup::GetObjCreationID(size_t index)
+	{
+		if (index < vecObjs.size())
+		{
+			if (vecObjs[index] != nullptr)
+			{
+				return vecObjs[index]->objData.GetCreationID();
+			}
+		}
+
+		return 0;
+	}
+
+	void ProjectileGroup::DeleteObj(size_t index)
+	{
+		delete vecObjs[index];
+		vecObjs[index] = nullptr;
+
+		vecObjs.erase(vecObjs.begin() + index);
+	}
+
+	size_t ProjectileGroup::GetOwnerCreationID(size_t index)
+	{
+		if (index < vecObjs.size())
+		{
+			if (vecObjs[index] != nullptr)
+			{
+				return vecObjs[index]->objData.GetOwnerID();
+			}
+		}
+
+		return 0;
+	}
+
+	void ProjectileGroup::CreateProjectiles(std::vector<CreateProjectile>& vecSpecs)
+	{
+		for (size_t i = 0; i < vecSpecs.size(); i++)
+		{
+			GameObj* obj = _Create();
+
+			if (vecSpecs[i].projectileType == ProjectileType::HADOUKEN)
+			{
+				obj->stateController.currentState = State::NewState<Hadouken_MoveForward>();
+			}
+
+			if (vecSpecs[i].forward.x < 0)
+			{
+				obj->objData.FaceRight(false);
+			}
+			else if (vecSpecs[i].forward.x > 0)
+			{
+				obj->objData.FaceRight(true);
+			}
+
+			obj->objData.SetPosition(vecSpecs[i].startPos);
+			obj->objData.SetOwnerID(vecSpecs[i].ownerObjCreationID);
+
+			vecObjs.push_back(obj);
+		}
+	}
+
+	GameObj* ProjectileGroup::_Create()
+	{
+		creationCount++;
+		GameObj* obj = new GameObj(creationCount);
+		return obj;
+	}
+}
--- FightingGameProject/ProjectileGroup.h
+++ FightingGameProject/ProjectileGroup.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "ObjGroup.h"
 #include "GameObj.h"
+#include "SheetRenderer.h"
 
 //projectiles
 #include "Hadouken_MoveForward.h"
@@ -14,202 +15,21 @@ namespace RB
 		size_t creationCount = 0;
 
 	public:
-		~ProjectileGroup()
-		{
-			if (vecObjs.size() != 0)
-			{
-				IF_COUT{ std::cout << std::endl; };
+		~ProjectileGroup();
 
-				for (size_t i = 0; i < vecObjs.size(); i++)
-				{
-					IF_COUT{ std::cout << "destructing projectile: " << vecObjs[i]->objData.GetCreationID() << std::endl; };
-					delete vecObjs[i];
-				}
+		void UpdateStates(GameData& gameData) override;
+		void RenderObjPosition(Camera& cam) override;
+		void RenderStates(Camera& cam, bool update) override;
+		void RenderBoxColliders(Camera& cam) override;
+		olc::vi2d GetObjWorldPos(size_t index) override;
+		olc::vi2d GetObjBoxColliderWorldPos(size_t index) override;
+		std::array<olc::vi2d, 4> GetObjBoxColliderWorldQuad(size_t index) override;
+		size_t GetObjCount() override;
+		size_t GetObjCreationID(size_t index) override;
+		void DeleteObj(size_t index) override;
+		size_t GetOwnerCreationID(size_t index) override;
 
-				IF_COUT{ std::cout << std::endl; };
-			}
-		}
-
-		void UpdateStates(GameData& gameData) override
-		{
-			for (size_t i = 0; i < vecObjs.size(); i++)
-			{
-				if (vecObjs[i] != nullptr)
-				{
-					GameObj& obj = *vecObjs[i];
-					State* state = vecObjs[i]->stateController.currentState;
-
-					if (state != nullptr)
-					{
-						state->RunUpdateProcess(vecObjs[i]->objData, gameData);
-					}
-				}
-			}
-		}
-
-		void RenderObjPosition(Camera& cam) override
-		{
-			for (size_t i = 0; i < vecObjs.size(); i++)
-			{
-				if (vecObjs[i] != nullptr)
-				{
-					vecObjs[i]->objData.objBoxCollider.Render(cam, vecObjs[i]->objData.GetPosition(), olc::CYAN);
-				}
-			}
-		}
-
-		void RenderStates(Camera& cam, bool update) override
-		{
-			for (size_t i = 0; i < vecObjs.size(); i++)
-			{
-				if (vecObjs[i] != nullptr)
-				{
-					SheetRenderer::Render(vecObjs[i], cam);
-
-					if (update)
-					{
-						vecObjs[i]->stateController.currentState->animationController.NextTileIndex();
-					}
-				}
-			}
-		}
-
-		void RenderBoxColliders(Camera& cam) override
-		{
-
-		}
-
-		olc::vi2d GetObjWorldPos(size_t index) override
-		{
-			if (index < vecObjs.size())
-			{
-				if (vecObjs[index] != nullptr)
-				{
-					return vecObjs[index]->objData.GetPosition();
-				}
-			}
-
-			return { 0, 0 };
-		}
-
-		olc::vi2d GetObjBoxColliderWorldPos(size_t index) override
-		{
-			if (index < vecObjs.size())
-			{
-				if (vecObjs[index] != nullptr)
-				{
-					olc::vi2d relativePos = vecObjs[index]->objData.objBoxCollider.RelativePosition();
-					olc::vi2d worldPos = relativePos + vecObjs[index]->objData.GetPosition();
-					return worldPos;
-				}
-			}
-
-			return { 0, 0 };
-		}
-
-		std::array<olc::vi2d, 4> GetObjBoxColliderWorldQuad(size_t index) override
-		{
-			std::array<olc::vi2d, 4> arr;
-
-			if (index < vecObjs.size())
-			{
-				if (vecObjs[index] != nullptr)
-				{
-					arr[0] = vecObjs[index]->objData.objBoxCollider.RelativePoint0();
-					arr[1] = vecObjs[index]->objData.objBoxCollider.RelativePoint1();
-					arr[2] = vecObjs[index]->objData.objBoxCollider.RelativePoint2();
-					arr[3] = vecObjs[index]->objData.objBoxCollider.RelativePoint3();
-
-					olc::vi2d relativePos = vecObjs[index]->objData.objBoxCollider.RelativePosition();
-
-					arr[0] += relativePos;
-					arr[1] += relativePos;
-					arr[2] += relativePos;
-					arr[3] += relativePos;
-
-					olc::vi2d playerPos = vecObjs[index]->objData.GetPosition();
-
-					arr[0] += playerPos;
-					arr[1] += playerPos;
-					arr[2] += playerPos;
-					arr[3] += playerPos;
-				}
-			}
-
-			return arr;
-		}
-
-		void CreateProjectiles(std::vector<CreateProjectile>& vecSpecs)
-		{
-			for (size_t i = 0; i < vecSpecs.size(); i++)
-			{
-				GameObj* obj = _Create();
-
-				if (vecSpecs[i].projectileType == ProjectileType::HADOUKEN)
-				{
-					obj->stateController.currentState = State::NewState<Hadouken_MoveForward>();
-				}
-
-				if (vecSpecs[i].forward.x < 0)
-				{
-					obj->objData.FaceRight(false);
-				}
-				else if (vecSpecs[i].forward.x > 0)
-				{
-					obj->objData.FaceRight(true);
-				}
-
-				obj->objData.SetPosition(vecSpecs[i].startPos);
-				obj->objData.SetOwnerID(vecSpecs[i].ownerObjCreationID);
-
-				vecObjs.push_back(obj);
-			}
-		}
-
-		size_t GetObjCount() override
-		{
-			return vecObjs.size();
-		}
-
-		GameObj* _Create()
-		{
-			creationCount++;
-			GameObj* obj = new GameObj(creationCount);
-			return obj;
-		}
-
-		size_t GetObjCreationID(size_t index) override
-		{
-			if (index < vecObjs.size())
-			{
-				if (vecObjs[index] != nullptr)
-				{
-					return vecObjs[index]->objData.GetCreationID();
-				}
-			}
-
-			return 0;
-		}
-
-		void DeleteObj(size_t index) override
-		{
-			delete vecObjs[index];
-			vecObjs[index] = nullptr;
-
-			vecObjs.erase(vecObjs.begin() + index);
-		}
-
-		size_t GetOwnerCreationID(size_t index) override
-		{
-			if (index < vecObjs.size())
-			{
-				if (vecObjs[index] != nullptr)
-				{
-					return vecObjs[index]->objData.GetOwnerID();
-				}
-			}
-
-			return 0;
-		}
+		void CreateProjectiles(std::vector<CreateProjectile>& vecSpecs);
+		GameObj* _Create();
 	};
 }
