a3bec34de246a0334c2904b7a320227e35ff2a2b
Choi <roundbeargames@gmail.com>
2021-04-18 17:33:27 +0900

    up in jump processor

--- FightingGameProject/JumpProcessor.h
+++ FightingGameProject/JumpProcessor.h
@@ -1,15 +1,36 @@
 #pragma once
 #include "GameData.h"
 #include "PlayerType.h"
+#include "PlayerInput.h"
 
 namespace RB
 {
 	class JumpProcessor
 	{
+	private:
+		size_t updateCount = 0;
+		int32_t upForce = 0;
+
 	public:
+		void SetUpForce(int32_t force)
+		{
+			upForce = force;
+		}
+
 		void UpdateJump(PlayerType playerType, GameData& gameData)
 		{
+			PlayerInput p = PlayerInput::Get(playerType, gameData);
 
+			if (p.up)
+			{
+				int n = 0;
+			}
+			else
+			{
+				int n = 0;
+			}
+
+			updateCount++;
 		}
 	};
 }
