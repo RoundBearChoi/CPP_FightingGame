e12c5e604059091328516a76f760c3aa9d5e1ea3
Choi <roundbeargames@gmail.com>
2021-04-27 13:08:28 +0900

    skipupdate function

--- FightingGameProject/SlowMotionMessage.h
+++ FightingGameProject/SlowMotionMessage.h
@@ -8,8 +8,23 @@ namespace RB
 	{
 	public:
 		SlowMotionGroupType groupType = SlowMotionGroupType::ALL;
+		size_t updateCount = 0;
 		int32_t interval = 0;
 		int32_t maxCount = 0;
-		int32_t currentCount = 0;
+
+		bool SkipUpdate(SlowMotionGroupType _groupType)
+		{
+			if (groupType == _groupType)
+			{
+				if (updateCount != 0 && updateCount % interval == 0)
+				{
+					return false;
+				}
+
+				updateCount++;
+
+				return true;
+			}
+		}
 	};
 }
