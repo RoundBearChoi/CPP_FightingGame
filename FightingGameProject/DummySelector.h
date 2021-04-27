353b2e0222423a6fff776b020b94bae5b4d9fa15
Choi <roundbeargames@gmail.com>
2021-04-27 22:14:51 +0900

    no need

--- FightingGameProject/DummySelector.h
+++ FightingGameProject/DummySelector.h
@@ -39,24 +39,29 @@ namespace RB
 				arrObjs[i] = new GameObj;
 			}
 
-			Init<Fighter_0_Idle>(*arrObjs[0]);
-			Init<Fighter_0_Jab>(*arrObjs[1]);
-			Init<Fighter_0_WalkForward>(*arrObjs[2]);
-			Init<Fighter_0_WalkBack>(*arrObjs[3]);
-			Init<Fighter_0_HitReaction_Side>(*arrObjs[4]);
-			Init<Fighter_0_HitReaction_Up>(*arrObjs[5]);
-			Init<Fighter_0_Hadouken_Fire>(*arrObjs[6]);
-			Init<Fighter_0_Hadouken_Recover>(*arrObjs[7]);
-			Init<Fighter_0_Jump_Prep_Vertical>(*arrObjs[8]);
-			Init<Fighter_0_Jump_Prep_Forward>(*arrObjs[9]);
-			Init<Fighter_0_Jump_Prep_Back>(*arrObjs[10]);
-			Init<Fighter_0_Jump_Up_Vertical>(*arrObjs[11]);
-			Init<Fighter_0_Jump_Up_Forward>(*arrObjs[12]);
-			Init<Fighter_0_Jump_Up_Back>(*arrObjs[13]);
-			Init<Fighter_0_Jump_Fall>(*arrObjs[14]);
-			Init<Fighter_0_Jump_WeakPunch>(*arrObjs[15]);
-			Init<Fighter_0_Crouch>(*arrObjs[16]);
-			Init<Fighter_0_Uppercut>(*arrObjs[17]);
+			arrObjs[0]->stateController.currentState = State::NewState<Fighter_0_Idle>();
+			arrObjs[1]->stateController.currentState = State::NewState<Fighter_0_Jab>();
+			arrObjs[2]->stateController.currentState = State::NewState<Fighter_0_WalkForward>();
+			arrObjs[3]->stateController.currentState = State::NewState<Fighter_0_WalkBack>();
+			arrObjs[4]->stateController.currentState = State::NewState<Fighter_0_HitReaction_Side>();
+			arrObjs[5]->stateController.currentState = State::NewState<Fighter_0_HitReaction_Up>();
+			arrObjs[6]->stateController.currentState = State::NewState<Fighter_0_Hadouken_Fire>();
+			arrObjs[7]->stateController.currentState = State::NewState<Fighter_0_Hadouken_Recover>();
+			arrObjs[8]->stateController.currentState = State::NewState<Fighter_0_Jump_Prep_Vertical>();
+			arrObjs[9]->stateController.currentState = State::NewState<Fighter_0_Jump_Prep_Forward>();
+			arrObjs[10]->stateController.currentState = State::NewState<Fighter_0_Jump_Prep_Back>();
+			arrObjs[11]->stateController.currentState = State::NewState<Fighter_0_Jump_Up_Vertical>();
+			arrObjs[12]->stateController.currentState = State::NewState<Fighter_0_Jump_Up_Forward>();
+			arrObjs[13]->stateController.currentState = State::NewState<Fighter_0_Jump_Up_Back>();
+			arrObjs[14]->stateController.currentState = State::NewState<Fighter_0_Jump_Fall>();
+			arrObjs[15]->stateController.currentState = State::NewState<Fighter_0_Jump_WeakPunch>();
+			arrObjs[16]->stateController.currentState = State::NewState<Fighter_0_Crouch>();
+			arrObjs[17]->stateController.currentState = State::NewState<Fighter_0_Uppercut>();
+
+			for (size_t i = 0; i < arrObjs.size(); i++)
+			{
+				arrObjs[i]->objData.SetOffsetType(OffsetType::BOTTOM_CENTER);
+			}
 
 			LoadColliders();
 		}
@@ -96,13 +101,6 @@ namespace RB
 			}
 
 			return arrObjs[currentIndex];
-		}
-
-		template<class T>
-		void Init(GameObj& obj)
-		{
-			obj.stateController.CreateNewState<T>();
-			obj.objData.SetOffsetType(OffsetType::BOTTOM_CENTER);
 		}
 
 		std::vector<BoxCollider>& GetCollider()
