706d5258769bb8b065f2df8db51d718c1d1c53c5
Choi <roundbeargames@gmail.com>
2021-05-10 22:40:58 +0900

    no need

--- FightingGameProject/DummySelector.h
+++ FightingGameProject/DummySelector.h
@@ -39,24 +39,24 @@ namespace RB
 				arrObjs[i] = new GameObj;
 			}
 
-			arrObjs[0]->stateController.currentState = State::NewState<Fighter_0_Idle>();
-			arrObjs[1]->stateController.currentState = State::NewState<Fighter_0_Jab>();
-			arrObjs[2]->stateController.currentState = State::NewState<Fighter_0_WalkForward>();
-			arrObjs[3]->stateController.currentState = State::NewState<Fighter_0_WalkBack>();
-			arrObjs[4]->stateController.currentState = State::NewState<Fighter_0_HitReaction_Side>();
-			arrObjs[5]->stateController.currentState = State::NewState<Fighter_0_HitReaction_Up>();
-			arrObjs[6]->stateController.currentState = State::NewState<Fighter_0_Hadouken_Fire>();
-			arrObjs[7]->stateController.currentState = State::NewState<Fighter_0_Hadouken_Recover>();
-			arrObjs[8]->stateController.currentState = State::NewState<Fighter_0_Jump_Prep_Vertical>();
-			arrObjs[9]->stateController.currentState = State::NewState<Fighter_0_Jump_Prep_Forward>();
-			arrObjs[10]->stateController.currentState = State::NewState<Fighter_0_Jump_Prep_Back>();
-			arrObjs[11]->stateController.currentState = State::NewState<Fighter_0_Jump_Up_Vertical>();
-			arrObjs[12]->stateController.currentState = State::NewState<Fighter_0_Jump_Up_Forward>();
-			arrObjs[13]->stateController.currentState = State::NewState<Fighter_0_Jump_Up_Back>();
-			arrObjs[14]->stateController.currentState = State::NewState<Fighter_0_Jump_Fall>();
-			arrObjs[15]->stateController.currentState = State::NewState<Fighter_0_Jump_WeakPunch>();
-			arrObjs[16]->stateController.currentState = State::NewState<Fighter_0_Crouch>();
-			arrObjs[17]->stateController.currentState = State::NewState<Fighter_0_Uppercut>();
+			arrObjs[0]->stateController.currentState = new Fighter_0_Idle();
+			arrObjs[1]->stateController.currentState = new Fighter_0_Jab();
+			arrObjs[2]->stateController.currentState = new Fighter_0_WalkForward();
+			arrObjs[3]->stateController.currentState = new Fighter_0_WalkBack();
+			arrObjs[4]->stateController.currentState = new Fighter_0_HitReaction_Side();
+			arrObjs[5]->stateController.currentState = new Fighter_0_HitReaction_Up();
+			arrObjs[6]->stateController.currentState = new Fighter_0_Hadouken_Fire();
+			arrObjs[7]->stateController.currentState = new Fighter_0_Hadouken_Recover();
+			arrObjs[8]->stateController.currentState = new Fighter_0_Jump_Prep_Vertical();
+			arrObjs[9]->stateController.currentState = new Fighter_0_Jump_Prep_Forward();
+			arrObjs[10]->stateController.currentState = new Fighter_0_Jump_Prep_Back();
+			arrObjs[11]->stateController.currentState = new Fighter_0_Jump_Up_Vertical();
+			arrObjs[12]->stateController.currentState = new Fighter_0_Jump_Up_Forward();
+			arrObjs[13]->stateController.currentState = new Fighter_0_Jump_Up_Back();
+			arrObjs[14]->stateController.currentState = new Fighter_0_Jump_Fall();
+			arrObjs[15]->stateController.currentState = new Fighter_0_Jump_WeakPunch();
+			arrObjs[16]->stateController.currentState = new Fighter_0_Crouch();
+			arrObjs[17]->stateController.currentState = new Fighter_0_Uppercut();
 
 			for (size_t i = 0; i < arrObjs.size(); i++)
 			{
