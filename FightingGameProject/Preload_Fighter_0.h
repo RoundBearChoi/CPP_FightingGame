55f84895d1fd7f137ac93e7e7aee2d9f975268d4
Choi <roundbeargames@gmail.com>
2021-05-11 15:09:55 +0900

    rename

--- FightingGameProject/Preload_Fighter_0.h
+++ FightingGameProject/Preload_Fighter_0.h
@@ -8,7 +8,7 @@ namespace RB
 	{
 	private:
 		template<class T>
-		T* Setup()
+		T* Load()
 		{
 			T* creator = new T();
 			State* newState = creator->Create();
@@ -21,24 +21,24 @@ namespace RB
 	public:
 		Preload_Fighter_0()
 		{
-			StateFactory::ptr->Creator_Fighter_0_Idle = Setup<CREATOR_Fighter_0_Idle>();
-			StateFactory::ptr->Creator_Fighter_0_WalkForward = Setup<CREATOR_Fighter_0_WalkForward>();
-			StateFactory::ptr->Creator_Fighter_0_WalkBack = Setup<CREATOR_Fighter_0_WalkBack>();
-			StateFactory::ptr->Creator_Fighter_0_Jab = Setup<CREATOR_Fighter_0_Jab>();
-			StateFactory::ptr->Creator_Fighter_0_HitReactionSide = Setup<CREATOR_Fighter_0_HitReactionSide>();
-			StateFactory::ptr->Creator_Fighter_0_HitReactionUp = Setup<CREATOR_Fighter_0_HitReactionUp>();
-			StateFactory::ptr->Creator_Fighter_0_Hadouken_Fire = Setup<CREATOR_Fighter_0_Hadouken_Fire>();
-			StateFactory::ptr->Creator_Fighter_0_Hadouken_Recover = Setup<CREATOR_Fighter_0_Hadouken_Recover>();
-			StateFactory::ptr->Creator_Fighter_0_Jump_Prep_Vertical = Setup<CREATOR_Fighter_0_Jump_Prep_Vertical>();
-			StateFactory::ptr->Creator_Fighter_0_Jump_Prep_Forward = Setup<CREATOR_Fighter_0_Jump_Prep_Forward>();
-			StateFactory::ptr->Creator_Fighter_0_Jump_Prep_Back = Setup<CREATOR_Fighter_0_Jump_Prep_Back>();
-			StateFactory::ptr->Creator_Fighter_0_Jump_Up_Vertical = Setup<CREATOR_Fighter_0_Jump_Up_Vertical>();
-			StateFactory::ptr->Creator_Fighter_0_Jump_Up_Forward = Setup<CREATOR_Fighter_0_Jump_Up_Forward>();
-			StateFactory::ptr->Creator_Fighter_0_Jump_Up_Back = Setup<CREATOR_Fighter_0_Jump_Up_Back>();
-			StateFactory::ptr->Creator_Fighter_0_Jump_Fall = Setup<CREATOR_Fighter_0_Jump_Fall>();
-			StateFactory::ptr->Creator_Fighter_0_Jump_WeakPunch = Setup<CREATOR_Fighter_0_Jump_WeakPunch>();
-			StateFactory::ptr->Creator_Fighter_0_Crouch = Setup<CREATOR_Fighter_0_Crouch>();
-			StateFactory::ptr->Creator_Fighter_0_Uppercut = Setup<CREATOR_Fighter_0_Uppercut>();
+			StateFactory::ptr->Creator_Fighter_0_Idle = Load<CREATOR_Fighter_0_Idle>();
+			StateFactory::ptr->Creator_Fighter_0_WalkForward = Load<CREATOR_Fighter_0_WalkForward>();
+			StateFactory::ptr->Creator_Fighter_0_WalkBack = Load<CREATOR_Fighter_0_WalkBack>();
+			StateFactory::ptr->Creator_Fighter_0_Jab = Load<CREATOR_Fighter_0_Jab>();
+			StateFactory::ptr->Creator_Fighter_0_HitReactionSide = Load<CREATOR_Fighter_0_HitReactionSide>();
+			StateFactory::ptr->Creator_Fighter_0_HitReactionUp = Load<CREATOR_Fighter_0_HitReactionUp>();
+			StateFactory::ptr->Creator_Fighter_0_Hadouken_Fire = Load<CREATOR_Fighter_0_Hadouken_Fire>();
+			StateFactory::ptr->Creator_Fighter_0_Hadouken_Recover = Load<CREATOR_Fighter_0_Hadouken_Recover>();
+			StateFactory::ptr->Creator_Fighter_0_Jump_Prep_Vertical = Load<CREATOR_Fighter_0_Jump_Prep_Vertical>();
+			StateFactory::ptr->Creator_Fighter_0_Jump_Prep_Forward = Load<CREATOR_Fighter_0_Jump_Prep_Forward>();
+			StateFactory::ptr->Creator_Fighter_0_Jump_Prep_Back = Load<CREATOR_Fighter_0_Jump_Prep_Back>();
+			StateFactory::ptr->Creator_Fighter_0_Jump_Up_Vertical = Load<CREATOR_Fighter_0_Jump_Up_Vertical>();
+			StateFactory::ptr->Creator_Fighter_0_Jump_Up_Forward = Load<CREATOR_Fighter_0_Jump_Up_Forward>();
+			StateFactory::ptr->Creator_Fighter_0_Jump_Up_Back = Load<CREATOR_Fighter_0_Jump_Up_Back>();
+			StateFactory::ptr->Creator_Fighter_0_Jump_Fall = Load<CREATOR_Fighter_0_Jump_Fall>();
+			StateFactory::ptr->Creator_Fighter_0_Jump_WeakPunch = Load<CREATOR_Fighter_0_Jump_WeakPunch>();
+			StateFactory::ptr->Creator_Fighter_0_Crouch = Load<CREATOR_Fighter_0_Crouch>();
+			StateFactory::ptr->Creator_Fighter_0_Uppercut = Load<CREATOR_Fighter_0_Uppercut>();
 		}
 	};
 }
