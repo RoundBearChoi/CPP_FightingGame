10afd5312521e4e64628523f85309636c9b7be5c
Choi <roundbeargames@gmail.com>
2021-05-11 15:08:59 +0900

    pointers

--- FightingGameProject/Preload_Fighter_0.h
+++ FightingGameProject/Preload_Fighter_0.h
@@ -22,23 +22,23 @@ namespace RB
 		Preload_Fighter_0()
 		{
 			StateFactory::ptr->Creator_Fighter_0_Idle = Setup<CREATOR_Fighter_0_Idle>();
-			Setup<CREATOR_Fighter_0_WalkForward>();
-			Setup<CREATOR_Fighter_0_WalkBack>();
-			Setup<CREATOR_Fighter_0_Jab>();
-			Setup<CREATOR_Fighter_0_HitReactionSide>();
-			Setup<CREATOR_Fighter_0_HitReactionUp>();
-			Setup<CREATOR_Fighter_0_Hadouken_Fire>();
-			Setup<CREATOR_Fighter_0_Hadouken_Recover>();
-			Setup<CREATOR_Fighter_0_Jump_Prep_Vertical>();
-			Setup<CREATOR_Fighter_0_Jump_Prep_Forward>();
-			Setup<CREATOR_Fighter_0_Jump_Prep_Back>();
-			Setup<CREATOR_Fighter_0_Jump_Up_Vertical>();
-			Setup<CREATOR_Fighter_0_Jump_Up_Forward>();
-			Setup<CREATOR_Fighter_0_Jump_Up_Back>();
-			Setup<CREATOR_Fighter_0_Jump_Fall>();
-			Setup<CREATOR_Fighter_0_Jump_WeakPunch>();
-			Setup<CREATOR_Fighter_0_Crouch>();
-			Setup<CREATOR_Fighter_0_Uppercut>();
+			StateFactory::ptr->Creator_Fighter_0_WalkForward = Setup<CREATOR_Fighter_0_WalkForward>();
+			StateFactory::ptr->Creator_Fighter_0_WalkBack = Setup<CREATOR_Fighter_0_WalkBack>();
+			StateFactory::ptr->Creator_Fighter_0_Jab = Setup<CREATOR_Fighter_0_Jab>();
+			StateFactory::ptr->Creator_Fighter_0_HitReactionSide = Setup<CREATOR_Fighter_0_HitReactionSide>();
+			StateFactory::ptr->Creator_Fighter_0_HitReactionUp = Setup<CREATOR_Fighter_0_HitReactionUp>();
+			StateFactory::ptr->Creator_Fighter_0_Hadouken_Fire = Setup<CREATOR_Fighter_0_Hadouken_Fire>();
+			StateFactory::ptr->Creator_Fighter_0_Hadouken_Recover = Setup<CREATOR_Fighter_0_Hadouken_Recover>();
+			StateFactory::ptr->Creator_Fighter_0_Jump_Prep_Vertical = Setup<CREATOR_Fighter_0_Jump_Prep_Vertical>();
+			StateFactory::ptr->Creator_Fighter_0_Jump_Prep_Forward = Setup<CREATOR_Fighter_0_Jump_Prep_Forward>();
+			StateFactory::ptr->Creator_Fighter_0_Jump_Prep_Back = Setup<CREATOR_Fighter_0_Jump_Prep_Back>();
+			StateFactory::ptr->Creator_Fighter_0_Jump_Up_Vertical = Setup<CREATOR_Fighter_0_Jump_Up_Vertical>();
+			StateFactory::ptr->Creator_Fighter_0_Jump_Up_Forward = Setup<CREATOR_Fighter_0_Jump_Up_Forward>();
+			StateFactory::ptr->Creator_Fighter_0_Jump_Up_Back = Setup<CREATOR_Fighter_0_Jump_Up_Back>();
+			StateFactory::ptr->Creator_Fighter_0_Jump_Fall = Setup<CREATOR_Fighter_0_Jump_Fall>();
+			StateFactory::ptr->Creator_Fighter_0_Jump_WeakPunch = Setup<CREATOR_Fighter_0_Jump_WeakPunch>();
+			StateFactory::ptr->Creator_Fighter_0_Crouch = Setup<CREATOR_Fighter_0_Crouch>();
+			StateFactory::ptr->Creator_Fighter_0_Uppercut = Setup<CREATOR_Fighter_0_Uppercut>();
 		}
 	};
 }
--- FightingGameProject/StateFactory.cpp
+++ FightingGameProject/StateFactory.cpp
@@ -12,5 +12,22 @@ namespace RB
 	StateFactory::~StateFactory()
 	{
 		delete Creator_Fighter_0_Idle;
+		delete Creator_Fighter_0_WalkForward;
+		delete Creator_Fighter_0_WalkBack;
+		delete Creator_Fighter_0_Jab;
+		delete Creator_Fighter_0_HitReactionSide;
+		delete Creator_Fighter_0_HitReactionUp;
+		delete Creator_Fighter_0_Hadouken_Fire;
+		delete Creator_Fighter_0_Hadouken_Recover;
+		delete Creator_Fighter_0_Jump_Prep_Vertical;
+		delete Creator_Fighter_0_Jump_Prep_Forward;
+		delete Creator_Fighter_0_Jump_Prep_Back;
+		delete Creator_Fighter_0_Jump_Up_Vertical;
+		delete Creator_Fighter_0_Jump_Up_Forward;
+		delete Creator_Fighter_0_Jump_Up_Back;
+		delete Creator_Fighter_0_Jump_Fall;
+		delete Creator_Fighter_0_Jump_WeakPunch;
+		delete Creator_Fighter_0_Crouch;
+		delete Creator_Fighter_0_Uppercut;
 	}
 }
