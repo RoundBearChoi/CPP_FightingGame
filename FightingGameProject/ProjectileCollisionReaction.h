f366072e775cbd6dacfa151e04c5e68b0ca32bd5
Choi <roundbeargames@gmail.com>
2021-06-06 18:57:33 +0900

    uppercut-like reaction on projectile midair hit

--- FightingGameProject/ProjectileCollisionReaction.h
+++ FightingGameProject/ProjectileCollisionReaction.h
@@ -27,22 +27,38 @@ namespace RB
 
 		void Update(int32_t fighterIndex, PlayerToProjectileCollisionResult collisionResult)
 		{
-			//midair hits todo
-			if (_fighters->GetObj(fighterIndex)->objData.GetPosition().y < 0)
+			if (collisionResult.isCollided)
 			{
+				//midair hits todo
+				if (_fighters->GetObj(fighterIndex)->objData.GetPosition().y < 0)
+				{
+					_projectiles->DeleteObj(collisionResult.projectileIndex);
+					_impactEffects->CreateObj(ObjType::HIT_EFFECT_0, collisionResult.midPoint);
 
-			}
-			//ground hits
-			else if (collisionResult.isCollided)
-			{
-				_projectiles->DeleteObj(collisionResult.projectileIndex);
-				_fighters->GetObj(fighterIndex)->SetNextState(State::NewState<Fighter_0_HitReaction_Side>(_fighters->GetObjData(fighterIndex)));
-				_impactEffects->CreateObj(ObjType::HIT_EFFECT_0, collisionResult.midPoint);
+					JumpSpecs jumpSpecs(10, 2, 1, true, true, false);
 
-				FightersHitStopMessage fightersHitStop;
-				ProjectilesHitStopMessage projectilesHitStop;
-				fightersHitStop.Register(12);
-				projectilesHitStop.Register(12);
+					_fighters->GetObj(fighterIndex)->objData.CreateNewJumpCalculator();
+					_fighters->GetObj(fighterIndex)->objData.ptrJumpCalculator->SetSpecs(jumpSpecs);
+					_fighters->GetObj(fighterIndex)->SetNextState(State::NewState<Fighter_0_HitReaction_Up>(_fighters->GetObjData(fighterIndex)));
+
+					FightersHitStopMessage fightersHitStop;
+					ProjectilesHitStopMessage projectilesHitStop;
+					fightersHitStop.Register(12);
+					projectilesHitStop.Register(12);
+				}
+				//ground hits
+				else
+				{
+					_projectiles->DeleteObj(collisionResult.projectileIndex);
+					_impactEffects->CreateObj(ObjType::HIT_EFFECT_0, collisionResult.midPoint);
+
+					_fighters->GetObj(fighterIndex)->SetNextState(State::NewState<Fighter_0_HitReaction_Side>(_fighters->GetObjData(fighterIndex)));
+					
+					FightersHitStopMessage fightersHitStop;
+					ProjectilesHitStopMessage projectilesHitStop;
+					fightersHitStop.Register(12);
+					projectilesHitStop.Register(12);
+				}
 			}
 		}
 	};
