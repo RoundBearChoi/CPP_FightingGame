29cae3fc715642c330f1f018952bce133f2594c2
Choi <roundbeargames@gmail.com>
2021-06-10 21:23:49 +0900

    drawing lines on previous position

--- FightingGameProject/PreviousPositionRenderer.h
+++ FightingGameProject/PreviousPositionRenderer.h
@@ -20,24 +20,38 @@ namespace RB
 
 		void Update() override
 		{
-			olc::vi2d bottomLeft = (*_vec)[0]->objData.objBoxCollider.RelativePoint1() + (*_vec)[0]->objData.GetPosition();
-			olc::vi2d bottomRight = (*_vec)[0]->objData.objBoxCollider.RelativePoint2() + (*_vec)[0]->objData.GetPosition();
+			DrawLines((*_vec)[0]);
+			DrawLines((*_vec)[1]);
+		}
 
-			olc::vi2d prevBottomLeft = (*_vec)[0]->objData.objBoxCollider.RelativePoint1() + (*_vec)[0]->objData.GetPreviousPosition();
-			olc::vi2d prevBottomRight = (*_vec)[0]->objData.objBoxCollider.RelativePoint2() + (*_vec)[0]->objData.GetPreviousPosition();
+		void DrawLines(GameObj* obj)
+		{
+			olc::vi2d bottomLeft = obj->objData.objBoxCollider.RelativePoint1() + obj->objData.GetPosition();
+			olc::vi2d bottomRight = obj->objData.objBoxCollider.RelativePoint2() + obj->objData.GetPosition();
+
+			olc::vi2d prevBottomLeft = obj->objData.objBoxCollider.RelativePoint1() + obj->objData.GetPreviousPosition();
+			olc::vi2d prevBottomRight = obj->objData.objBoxCollider.RelativePoint2() + obj->objData.GetPreviousPosition();
 
 			if (bottomLeft != prevBottomLeft)
 			{
 				olc::vi2d screenPos_bottomLeft = ScreenVector::GetScreenPosition(bottomLeft, *_cam);
 				olc::vi2d screenPos_prevBottomLeft = ScreenVector::GetScreenPosition(prevBottomLeft, *_cam);
-				olc::Renderer::ptrPGE->DrawLine(screenPos_bottomLeft, screenPos_prevBottomLeft, olc::CYAN);
+
+				olc::vi2d dir = screenPos_prevBottomLeft - screenPos_bottomLeft;
+				olc::vi2d normDir = dir.norm();
+
+				olc::Renderer::ptrPGE->DrawLine(screenPos_bottomLeft + (dir * 5), screenPos_bottomLeft - (dir * 5), olc::CYAN);
 			}
 
 			if (bottomRight != prevBottomRight)
 			{
 				olc::vi2d screenPos_bottomRight = ScreenVector::GetScreenPosition(bottomRight, *_cam);
 				olc::vi2d screenPos_prevBottomRight = ScreenVector::GetScreenPosition(prevBottomRight, *_cam);
-				olc::Renderer::ptrPGE->DrawLine(screenPos_bottomRight, screenPos_prevBottomRight, olc::CYAN);
+
+				olc::vi2d dir = screenPos_prevBottomRight - screenPos_bottomRight;
+				olc::vi2d normDir = dir.norm();
+
+				olc::Renderer::ptrPGE->DrawLine(screenPos_bottomRight + (dir * 5), screenPos_bottomRight - (dir * 5), olc::CYAN);
 			}
 		}
 	};
