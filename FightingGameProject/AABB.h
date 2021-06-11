b6f98bf51e73e4c1c7da5ee94f4f717b8e7f66fa
Choi <roundbeargames@gmail.com>
2021-06-11 20:51:16 +0900

    full aabb

--- FightingGameProject/AABB.h
+++ FightingGameProject/AABB.h
@@ -11,14 +11,14 @@ namespace RB
 		{
 			std::array<olc::vi2d, 4> p1Quad;
 			p1Quad[0] = boxCol1.RelativePoint0() + p1Position;
-			//p1Quad[1] = boxCol1.RelativePoint1() + p1Position;
-			//p1Quad[2] = boxCol1.RelativePoint2() + p1Position;
+			p1Quad[1] = boxCol1.RelativePoint1() + p1Position;
+			p1Quad[2] = boxCol1.RelativePoint2() + p1Position;
 			p1Quad[3] = boxCol1.RelativePoint3() + p1Position;
 
 			std::array<olc::vi2d, 4> p2Quad;
 			p2Quad[0] = boxCol2.RelativePoint0() + p2Position;
-			//p2Quad[1] = boxCol2.RelativePoint1() + p2Position;
-			//p2Quad[2] = boxCol2.RelativePoint2() + p2Position;
+			p2Quad[1] = boxCol2.RelativePoint1() + p2Position;
+			p2Quad[2] = boxCol2.RelativePoint2() + p2Position;
 			p2Quad[3] = boxCol2.RelativePoint3() + p2Position;
 
 			if (p1Quad[0].x < p2Quad[3].x && //top left vs top right
